2e7065bb982fb322c380aa86290a60e6f5d7420b
matklad <aleksey.kladov@gmail.com>
2013-02-25 13:31:30 +0400

    Some database structures
    docs/stages.txt - description of exposition stages
    flowers/exposition/models/py - basic database layout

--- flowers/exposition/models.py
+++ flowers/exposition/models.py
@@ -1,6 +1,34 @@
 from django.db import models
+import itertools
+
+
+class Room(models.Model):
+    area = models.IntegerField()
 
 
 class Exposition(models.Model):
+    STAGES = zip(itertools.count(), ["idea",
+                                     "plant_selection",
+                                     "design",
+                                     "verification",
+                                     "approval",
+                                     "ready",
+                                     "active",
+                                     "archive"])
+
     name = models.CharField(max_length=100)
     description = models.TextField()
+    room = models.ForeignKey(Room)
+    stage = models.SmallIntegerField(choices=STAGES)
+    begin = models.DateField()
+    end = models.DateField()
+    plants = models.ManyToManyField(Plant)
+
+
+class PlantSpecies(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Plant(models.Model):
+    species = models.ForeignKey(PlantSpecies)
--- flowers/exposition/views.py
+++ flowers/exposition/views.py
@@ -14,16 +14,16 @@ class AddExpositionForm(forms.Form):
 
 @user_passes_test(lambda u: u in Group.objects.get(name="manager").user_set.all())
 def create_exposition(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = AddExpositionForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
+    if request.method == 'POST':  # If the form has been submitted...
+        form = AddExpositionForm(request.POST)  # A form bound to the POST data
+        if form.is_valid():  # All validation rules pass
             exp = Exposition()
             exp.name = form.data['name']
             exp.description = form.data['description']
             exp.save()
             return HttpResponseRedirect('') # Redirect after POST
     else:
-        form = AddExpositionForm() # An unbound form
+        form = AddExpositionForm()  # An unbound form
 
     return render(request, 'create_exposition.html', {
         'form': form,
