124d140d1061584a1fc7503eb655ec5f225ca868
Aleksey Kladov <aleksey.kladov@gmail.com>
2013-03-05 21:46:57 +0400

    fix

--- flowers/exposition/urls.py
+++ flowers/exposition/urls.py
@@ -5,7 +5,7 @@ from exposition.views import create_exposition
 
 urlpatterns = patterns('',
                        url(r'^list/$', ListView.as_view(
-                           queryset=Exposition.objects.all(),
+                           queryset=Exposition.objects.all,
                            template_name='exposition_list.html')),
                        url(r'^create_exposition/$', create_exposition))
 
