eb0d68f99787e05ea6ff970351f14757c3115484
Aleksey Kladov <aleksey.kladov@gmail.com>
2013-03-06 02:22:25 +0400

    exposition details stub

--- flowers/apps/exposition/urls.py
+++ flowers/apps/exposition/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import patterns, url
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, DetailView
 from exposition.models import Exposition
 from exposition.views import create_exposition
 
 urlpatterns = patterns('',
                        url(r'^list/$',
                            ListView.as_view(
-                           queryset=Exposition.objects.all(),
-                           template_name='exposition/exposition_list.html'),
+                               queryset=Exposition.objects.all(),
+                               template_name='exposition/exposition_list.html'),
                            name='exposition_list'),
-                       url(r'^create_exposition/$', create_exposition))
+                       url(r'^create_exposition/$', create_exposition),
+                       url(r'^(?P<pk>\d+)/$',
+                           DetailView.as_view(model=Exposition, template_name='exposition/exposition_details.html'),
+                           name="exposition_detail"))
 
 
