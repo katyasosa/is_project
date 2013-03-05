261bff2e04b00685f695e05768d0c872a673c0d8
Aleksey Kladov <aleksey.kladov@gmail.com>
2013-03-06 01:03:32 +0400

    fixed logaut redirect?

--- flowers/apps/employees/urls.py
+++ flowers/apps/employees/urls.py
@@ -3,4 +3,4 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
                        url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'employees/login.html'},
                            name='login_form'),
-                       url(r'^logout/$', 'django.contrib.auth.views.logout'))
+                       url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}))
