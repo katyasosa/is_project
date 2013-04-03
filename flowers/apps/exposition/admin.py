f3c15aaa59f06060a01d66dde6682e2982fa10a5
matklad <aleksey.kladov@gmail.com>
2013-04-03 14:24:42 +0400

    deleting old flowers from git

--- flowers/apps/exposition/admin.py
+++
@@ -1,7 +0,0 @@
-from exposition.models import Exposition, Room, PlantSpecies, Plant
-from django.contrib import admin
-
-admin.site.register(Exposition)
-admin.site.register(Room)
-admin.site.register(PlantSpecies)
-admin.site.register(Plant)
