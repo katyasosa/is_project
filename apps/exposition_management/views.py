92eeb5cc1073fa0a7e149e8a37b2172f57e4e743
matklad <aleksey.kladov@gmail.com>
2013-04-03 15:14:20 +0400

    CreateExpositionView fixes
    exclude "plants", add get_success_url

--- apps/exposition_management/views.py
+++ apps/exposition_management/views.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.views.generic import CreateView
 from django import forms
 
@@ -8,8 +9,12 @@ class AddExpositionForm(forms.ModelForm):
 
     class Meta:
         model = Exposition
+        exclude = ['plants']
 
 class CreateExpositionView(CreateView):
     model = Exposition
     template_name = 'exposition_management/create_exposition.html'
     form_class = AddExpositionForm
+
+    def get_success_url(self):
+        return reverse('exposition_list')
