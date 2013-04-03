2cf49e0d6528978fb134618340d12abe3b656846
matklad <aleksey.kladov@gmail.com>
2013-04-03 16:22:45 +0400

    __str__ for models

--- apps/exposition_management/models.py
+++ apps/exposition_management/models.py
@@ -1,19 +1,32 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Room(models.Model):
     area = models.IntegerField()
 
+    def __str__(self):
+        return str(self.area) + " area room"
 
+
+@python_2_unicode_compatible
 class PlantSpecies(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
+@python_2_unicode_compatible
 class Plant(models.Model):
     species = models.ForeignKey(PlantSpecies)
 
+    def __str__(self):
+        return str(self.species) + " plant"
 
+
+@python_2_unicode_compatible
 class Exposition(models.Model):
     STAGE_IDEA = 1
 
@@ -25,4 +38,5 @@ class Exposition(models.Model):
     end = models.DateField()
     plants = models.ManyToManyField(Plant)
 
-
+    def __str__(self):
+        return self.name
