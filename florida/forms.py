da823ce44c3608b99af62b3ea1f38a76e62e6854
Sergei Lebedev <superbobry@gmail.com>
2013-04-11 04:52:17 +0400

    Fixed redirect on login -- should've been '/'

--- florida/forms.py
+++ florida/forms.py
@@ -1,10 +1,11 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Hidden, Submit
 from django.contrib.auth.forms import AuthenticationForm
 
 
 class CrispyAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
+        self.helper.add_input(Hidden('next', '/'))
         self.helper.add_input(Submit('submit', 'Login'))
         super(CrispyAuthenticationForm, self).__init__(*args, **kwargs)
